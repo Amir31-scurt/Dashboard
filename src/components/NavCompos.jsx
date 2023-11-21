84e3a4b6fc696951fd49a891bbc08171f2401707
barrymamadou12345 <dioungol5432@gmail.com>
2023-11-21 18:13:43 +0000

    Travail

--- src/components/NavCompos.jsx
+++ src/components/NavCompos.jsx
@@ -1,32 +1,32 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { AiOutlineBell } from "react-icons/ai";
 import { IoIosArrowDown } from "react-icons/io";
-function NavBar(){
-    // state///////////
+function NavBar() {
+  // state///////////
 
   // comportement/////////
 
   // affichage //////////////
 
-    return (
-        <header className=" flex items-center justify-center h-20">
-          <nav className="flex flex-cols justify-between w-11/12">
-            <div className="flex gap-4 items-center">
-            <FaMagnifyingGlass className="text-gray-500"/>
-            <input type="text" placeholder="Search Data" className="w-28 inp"/>
-            </div>
-            <div className="flex gap-8 items-center">
-            <AiOutlineBell className="cloche"/>
-            <div className="flex items-center gap-4">
-                <img src="Images/User.jpg" alt=""  className="w-12 rounded-full"/>
-                <h2 className="inp">Kalika</h2>
-            </div>
-            <IoIosArrowDown className="cloche"/>
-            </div>
-          </nav>
-        </header>
-      )
-    
+  return (
+    <header className=" flex items-center justify-center h-20">
+      <nav className="flex flex-cols justify-between w-11/12">
+        <div className="flex gap-4 items-center">
+          <FaMagnifyingGlass className="text-gray-500" />
+          <input type="text" placeholder="Search Data" className="w-28 inp" />
+        </div>
+        <div className="flex gap-8 items-center">
+          <AiOutlineBell className="cloche" />
+          <div className="flex items-center gap-4">
+            <img src="Images/User.jpg" alt="" className="w-12 rounded-full" />
+            <h2 className="inp">Kalika</h2>
+          </div>
+          <IoIosArrowDown className="cloche" />
+        </div>
+      </nav>
+    </header>
+  )
+
 }
 
 export default NavBar;
--- src/components/SidCompos.jsx
+++ src/components/SidCompos.jsx
@@ -1,4 +1,5 @@
 
+import { AiOutlineBell } from "react-icons/ai";
 export default function SideBar() {
   // state///////////
 
@@ -6,8 +7,8 @@ export default function SideBar() {
 
   // affichage //////////////
   return (
-    <div className=" flex items-center justify-center h-20">
-      <div className="flex items-center w-3/4">
+    <div className=" flex flex-col items-center justify-center">
+      <div className="flex items-center w-3/4 mt-3">
         <div className="imge">
           <img src="Images/logoBakeli.jpg" className="" alt="" />
         </div>
@@ -16,6 +17,58 @@ export default function SideBar() {
           <p className="text-sm ms-4 text-gray-500 fonto">School of Technology</p>
         </div>
       </div>
+
+      {/************* Menu ************/}
+
+      <div className=" mt-10 longuer w-3/4">
+
+        <div className=" flex-col flex gap-2">{/*** Main-Menu ***/}
+          <div className="mb-4">
+            <h1 className="text-gray-500 font-bold ">MAIN MENU</h1>
+          </div>
+
+          <div className="flex-col click">
+            <button className="flex my-4 px-6 items-center ">
+              <AiOutlineBell className="me-6 icono" />
+              <h2 className="text-xl font-bold ">Overview</h2>
+            </button>
+          </div>
+
+          <div className="flex-col click">
+            <button className="flex my-4 px-6 items-center ">
+              <AiOutlineBell className="me-6 icono" />
+              <h2 className="text-xl font-bold ">Overview</h2>
+            </button>
+          </div>
+
+          <div className="flex-col click">
+            <button className="flex my-4 px-6 items-center ">
+              <AiOutlineBell className="me-6 icono" />
+              <h2 className="text-xl font-bold ">Overview</h2>
+            </button>
+          </div>
+
+          <div className="flex-col click">
+            <button className="flex my-4 px-6 items-center ">
+              <AiOutlineBell className="me-6 icono" />
+              <h2 className="text-xl font-bold ">Overview</h2>
+            </button>
+          </div>
+
+   
+        </div>
+
+
+        <div className="">{/*** Reference-Menu ***/}
+
+        </div>
+
+
+        <div className="">{/*** Notice-Menu ***/}
+
+        </div>
+      </div>
+
     </div>
   )
 }
