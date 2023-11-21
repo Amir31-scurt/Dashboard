2f96defcd96e51ab0c2ae3bf965a96776b71cfb8
barrymamadou12345 <dioungol5432@gmail.com>
2023-11-21 14:49:43 +0000

    Tl

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './index.css'
 
-<<<<<<< HEAD
 import './CSS/Style-App.css'
 import SideBar from './components/SidCompos';
-=======
 import SideBar from './components/SidCompos';
 import './CSS/Style-App.css'
->>>>>>> d2897351a29a47e472ca58562846a7161f154b1b
+
+
 
 function App() {
   //State {etat, donnees dynamiques}//////////////////////
